eaa2093ee7c71a53a6fbc2e552cec4bf36f66591
Ashish Job <ashishjob104@gmail.com>
2023-08-11 00:24:56 -0500

    refactor: fix errors

--- app/mobileProjects/page.tsx
+++ app/mobileProjects/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { projects } from "../../components/Projects";
+
 const MobileProjects = () => {
   return (
     <div className="my-24">
@@ -8,34 +9,31 @@ const MobileProjects = () => {
       </h1>
       <div className="my-12 h-8 mx-20 md:mx-40 bg-center rounded-2xl bg-white"></div>
       <ul className="flex-col">
-          {projects.map((projects, ind) => {
-            return (
-              <MobileProjectsCard key={ind}>
-                <a href={projects.url} target="_blank">
-                  <p className="font-bold text-2xl hover:text-lightgreen text-center mb-3 underline mx-4">
-                    {projects.title}
-                  </p>
-                  <p className="font-semibold text-xl text-center my-3 mx-4">
-                    {projects.description}
-                  </p>
-                  <p className="text-center mt-3 text-lg mx-4">{projects.time}</p>
-                  <div className="flex flex-row justify-center">
-                    {projects.icons.map((icon, icon_ind) => {
-                      return (
-                        <Image
-                          src={`/${icon}.svg`}
-                          width="35"
-                          height="35"
-                          alt={icon}
-                          className="mr-3 my-2 mb-12 items-center justify-center"
-                        />
-                      );
-                    })}
-                  </div>
-                </a>
-              </MobileProjectsCard>
-            );
-          })}
+        {projects.map((project, ind) => (
+          <MobileProjectsCard key={ind}>
+            <a href={project.url} target="_blank" rel="noopener noreferrer">
+              <p className="font-bold text-2xl hover:text-lightgreen text-center mb-3 underline mx-4">
+                {project.title}
+              </p>
+              <p className="font-semibold text-xl text-center my-3 mx-4">
+                {project.description}
+              </p>
+              <p className="text-center mt-3 text-lg mx-4">{project.time}</p>
+              <div className="flex flex-row justify-center">
+                {project.icons.map((icon, icon_ind) => (
+                  <Image
+                    key={icon_ind}
+                    src={`/${icon}.svg`}
+                    width={35}
+                    height={35}
+                    alt={icon}
+                    className="mr-3 my-2 mb-12 items-center justify-center"
+                  />
+                ))}
+              </div>
+            </a>
+          </MobileProjectsCard>
+        ))}
       </ul>
     </div>
   );
@@ -44,7 +42,6 @@ const MobileProjects = () => {
 export default MobileProjects;
 
 interface MobileProjectsProps {
-  key: number;
   children: React.ReactNode;
 }
 
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,53 +1,50 @@
 import Image from "next/image";
 import { projects } from "../../components/Projects";
-const Projects = () => {
+
+const MobileProjects = () => {
   return (
     <div className="my-24">
-      <h1 className="hover:text-lightgreen w-fit text-8xl underline">
+      <h1 className="hover:text-lightgreen text-center text-4xl underline">
         projects.
       </h1>
-      <div className="mb-16 mt-24 h-8 w-11/12 rounded-2xl bg-white"></div>
-      <ul className="flex justify-evenly">
-          {projects.map((projects, ind) => {
-            return (
-              <ProjectsCard key={ind}>
-                <a href={projects.url} target="_blank">
-                  <p className="hover:text-lightgreen mb-3 w-2/3 text-3xl font-bold underline">
-                    {projects.title}
-                  </p>
-                  <p className="my-3 w-2/3 text-2xl font-semibold">
-                    {projects.description}
-                  </p>
-                  <p className="my-3 text-xl font-semibold">{projects.time}</p>
-                  <div className="flex flex-row">
-                    {projects.icons.map((icon, icon_ind) => {
-                      return (
-                        <Image
-                          src={`/${icon}.svg`}
-                          width="35"
-                          height="35"
-                          alt={icon}
-                          className="mr-3"
-                        />
-                      );
-                    })}
-                  </div>
-                </a>
-              </ProjectsCard>
-            );
-          })}
+      <div className="my-12 h-8 mx-20 md:mx-40 bg-center rounded-2xl bg-white"></div>
+      <ul className="flex-col">
+        {projects.map((project, ind) => (
+          <MobileProjectsCard key={ind}>
+            <a href={project.url} target="_blank" rel="noopener noreferrer">
+              <p className="font-bold text-2xl hover:text-lightgreen text-center mb-3 underline mx-4">
+                {project.title}
+              </p>
+              <p className="font-semibold text-xl text-center my-3 mx-4">
+                {project.description}
+              </p>
+              <p className="text-center mt-3 text-lg mx-4">{project.time}</p>
+              <div className="flex flex-row justify-center">
+                {project.icons.map((icon, icon_ind) => (
+                  <Image
+                    key={icon_ind}
+                    src={`/${icon}.svg`}
+                    width={35}
+                    height={35}
+                    alt={icon}
+                    className="mr-3 my-2 mb-12 items-center justify-center"
+                  />
+                ))}
+              </div>
+            </a>
+          </MobileProjectsCard>
+        ))}
       </ul>
     </div>
   );
 };
 
-export default Projects;
+export default MobileProjects;
 
-interface ProjectsProps {
-  key: number;
+interface MobileProjectsProps {
   children: React.ReactNode;
 }
 
-const ProjectsCard = (props: ProjectsProps) => {
+const MobileProjectsCard = (props: MobileProjectsProps) => {
   return <li>{props.children}</li>;
 };
